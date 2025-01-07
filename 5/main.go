df60aab25aa923e0a2628c48d293f353ccbbfe6d
Gin <gkolomytcyn@gmail.com>
2025-01-07 15:38:06 +0200

    finishing day 5 part two

--- 5/main.go
+++ 5/main.go
@@ -10,6 +10,9 @@ import (
 
 func main() {
 	sum := 0
+	broken_sum := 0
+	broken_lines := []string{}
+
 	matrix, lines := readFile()
 
 	// fmt.Println(matrix)
@@ -24,6 +27,10 @@ func main() {
 			remain := pages[i+1:]
 			isValid = lookupVal(page_num, remain, matrix)
 			if !isValid {
+				// not valid
+				// take the line and pass to fix method and get the middle
+				broken_lines = append(broken_lines, input_list)
+				// broken_sum += fixOrderAndGetMiddle(i, pages, matrix)
 				break
 			}
 		}
@@ -36,23 +43,57 @@ func main() {
 		}
 	}
 
+	fmt.Println("Broken lines")
+	for _, i := range broken_lines {
+		fixed_line := fixOrder(strings.Split(i, ","), matrix)
+		broken_sum += getMiddle(fixed_line)
+	}
+
 	fmt.Printf("\r\n\r\nThe sum is: %v\r\n", sum)
+	fmt.Printf("\r\n\r\nThe broken sum is: %v\r\n", broken_sum)
 }
 
 func lookupVal(val string, items []string, matrix map[string][]string) bool {
 	for _, i := range items {
-		key, ok := matrix[i]
+		value, ok := matrix[i]
 		if !ok {
 			return true
 		}
 
-		for _, num := range key {
+		for _, num := range value {
 			if num == val {
 				return false
 			}
 		}
 	}
 	return true
+}
+
+func fixOrder(line []string, matrix map[string][]string) []string {
+	swapped := false
+	for val := 0; val < len(line); val++ {
+		for i := val + 1; i < len(line); i++ {
+			allowed, ok := matrix[line[i]]
+			if !ok {
+				continue
+			}
+
+			for _, r := range allowed {
+				if r == line[val] {
+					tmp := line[val]
+					line[val] = line[i]
+					line[i] = tmp
+					swapped = true
+				}
+			}
+		}
+		if swapped {
+			swapped = false
+			val--
+		}
+	}
+
+	return line
 }
 
 func getMiddle(items []string) int {
