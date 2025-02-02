8a416814cab5129c28af09d66210f4af45f4baa0
Gin <gkolomytcyn@gmail.com>
2025-02-02 20:26:15 +0200

    finishing day 8 part two

--- 8/main.go
+++ 8/main.go
@@ -10,7 +10,8 @@ import (
 )
 
 func main() {
-	antenas := make(map[string]int)
+	antennas := make(map[string]int)
+	antennas_part_two := make(map[string]int)
 	coords, x_max, y_max := readFile()
 
 	// loop over the corrds
@@ -20,30 +21,69 @@ func main() {
 			continue
 		}
 
+		// match every antenna of the same frequency (excluding itself)
 		for x := 0; x < len(coord_list); x++ {
 			for y := x + 1; y < len(coord_list); y++ {
-				first_diff, err1 := getDiff(coord_list[x], coord_list[y], x_max, y_max)
-				second_diff, err2 := getDiff(coord_list[y], coord_list[x], x_max, y_max)
+				// get the increasing and decreasing difference
+				first_diff := getDiff(coord_list[x], coord_list[y])
+				second_diff := getDiff(coord_list[y], coord_list[x])
+
+				// get the coords from the difference
+				inc_coords, err1 := getCoords(coord_list[x], first_diff, x_max, y_max)
+				desc_coords, err2 := getCoords(coord_list[y], second_diff, x_max, y_max)
 
 				if err1 == nil {
-					// add the coords to the listA
-					first_coords := strings.Join(first_diff, ",")
-					antenas[first_coords]++
+					antennas[inc_coords]++
+					antennas_part_two[inc_coords]++
 				}
 
 				if err2 == nil {
-					// add the coords to the listA
-					second_coors := strings.Join(second_diff, ",")
-					antenas[second_coors]++
+					antennas[desc_coords]++
+					antennas_part_two[desc_coords]++
+				}
+
+				// part two - loop adding/subtracting the difference until out of bounds
+				rec_inc_coords := inc_coords
+				if inc_coords != "" {
+					for {
+						out, err := getCoords(rec_inc_coords, first_diff, x_max, y_max)
+						if err != nil {
+							break
+						}
+						antennas_part_two[out]++
+						rec_inc_coords = out
+					}
+				}
+
+				rec_desc_coords := desc_coords
+				if desc_coords != "" {
+					for {
+						out, err := getCoords(rec_desc_coords, second_diff, x_max, y_max)
+						if err != nil {
+							break
+						}
+						antennas_part_two[out]++
+						rec_desc_coords = out
+					}
 				}
 			}
 		}
 	}
 
-	fmt.Printf("len is: %v\r\n", len(antenas))
+	fmt.Printf("part one is: %v\r\n", len(antennas))
+
+	// adding the existing antennas to the count
+	for _, arr := range coords {
+		for _, i := range arr {
+			antennas_part_two[i]++
+		}
+	}
+
+	fmt.Printf("part two len is:%v\r\n", len(antennas_part_two))
 }
 
-func getDiff(s1, s2 string, x_max, y_max int) ([]string, error) {
+// calculate the difference/gradient between coords
+func getDiff(s1, s2 string) []string {
 	// split the numbers
 	one := strings.Split(s1, ",")
 	two := strings.Split(s2, ",")
@@ -59,18 +99,30 @@ func getDiff(s1, s2 string, x_max, y_max int) ([]string, error) {
 	x_diff := x1 - x2
 	y_diff := y1 - y2
 
-	x1 = x1 + x_diff
-	y1 = y1 + y_diff
+	return []string{strconv.Itoa(x_diff), strconv.Itoa(y_diff)}
+}
 
-	if x1 < 0 || y1 < 0 {
-		return []string{}, errors.New("invalid coords")
+// add gradient to the coords
+func getCoords(og_coords string, diff []string, x_max, y_max int) (string, error) {
+	split := strings.Split(og_coords, ",")
+	x, _ := strconv.Atoi(split[0])
+	y, _ := strconv.Atoi(split[1])
+
+	x_diff, _ := strconv.Atoi(diff[0])
+	y_diff, _ := strconv.Atoi(diff[1])
+
+	x = x + x_diff
+	y = y + y_diff
+
+	if x < 0 || y < 0 {
+		return "", errors.New("invalid coords")
 	}
 
-	if x1 >= x_max || y1 >= y_max {
-		return []string{}, errors.New("invalid coords")
+	if x >= x_max || y >= y_max {
+		return "", errors.New("invalid coords")
 	}
 
-	return []string{strconv.Itoa(x1), strconv.Itoa(y1)}, nil
+	return fmt.Sprintf("%v,%v", strconv.Itoa(x), strconv.Itoa(y)), nil
 }
 
 func readFile() (map[string][]string, int, int) {
