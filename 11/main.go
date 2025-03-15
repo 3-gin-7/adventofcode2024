b538e42251ce86680f65976ab4a6e467c2aec5e4
Gin <gkolomytcyn@gmail.com>
2025-03-15 17:34:36 +0200

    trying to switch to dictionaries- failed

--- 11/main.go
+++ 11/main.go
@@ -11,12 +11,28 @@ import (
 func main() {
 	stones := readFile()
 	fmt.Println(stones.listToSlice())
-	count := 0
+	seen_numbers := make(map[string][]string)
 
-	for j := 0; j < stones.length; j++ {
-		count += test(0, stones.getAtIndex(j).data, 1)
+	for i := 0; i < 75; i++ {
+		fmt.Printf("iteration %v: stones count: %v\r\n", i, stones.length)
+		for j := 0; j < stones.length; j++ {
+			node := stones.getAtIndex(j)
+			transformed_stones := strings.Split(node.data, " ")
+
+			value, ok := seen_numbers[node.data]
+			if ok {
+				transformed_stones = value
+			} else {
+				transformed_stones = applyRulesTwo(transformed_stones)
+				seen_numbers[node.data] = transformed_stones
+			}
+
+			stones.insertRangeAtIndex(transformed_stones, j)
+			j += len(transformed_stones) - 1
+		}
 	}
-	fmt.Println(count)
+
+	fmt.Println(stones.length)
 }
 
 func test(blink int, stones string, stone_count int) int {
@@ -24,19 +40,31 @@ func test(blink int, stones string, stone_count int) int {
 		return stone_count
 	}
 
-	transformed_stone := applyRulesTwo(stones)
+	transformed_stone := applyRules(stones)
 	if len(transformed_stone) > 1 {
 		stone_count++
 	}
 
 	for _, i := range transformed_stone {
-		stone_count = test(blink+1, i, stone_count)
+		if i != "1" && i != "0" {
+			stone_count = test(blink+1, i, stone_count)
+		}
 	}
 
 	return stone_count
 }
 
-func applyRulesTwo(data string) []string {
+func applyRulesTwo(data []string) []string {
+	output := []string{}
+	for _, i := range data {
+		new_data := applyRules(i)
+		output = append(output, new_data...)
+	}
+
+	return output
+}
+
+func applyRules(data string) []string {
 	int_data, _ := strconv.Atoi(data)
 	if int_data == 0 {
 		return []string{"1"}
@@ -54,8 +82,8 @@ func applyRulesTwo(data string) []string {
 }
 
 func readFile() *LinkedList {
-	FILE_NAME := "test.txt"
-	// FILE_NAME := "input.txt"
+	// FILE_NAME := "test.txt"
+	FILE_NAME := "input.txt"
 	fi, err := os.Open(FILE_NAME)
 	if err != nil {
 		panic(err)
@@ -114,6 +142,28 @@ func (list *LinkedList) insertAtIndex(data []string, index int) {
 	future_next := insert_node.next
 	insert_node.data = data[0]
 	insert_node.next = &Node{data: data[1], next: future_next}
+}
+
+func (list *LinkedList) insertRangeAtIndex(data []string, index int) {
+	if list.length <= index {
+		panic("index is out of range")
+	}
+	list.length += len(data) - 1
+
+	insert_node := list.head
+	for i := 0; i < index; i++ {
+		insert_node = insert_node.next
+	}
+
+	insert_node.data = data[0]
+	link_node := insert_node.next
+	for i := 1; i < len(data); i++ {
+		new_node := &Node{data: data[i], next: link_node}
+		insert_node.next = new_node
+		insert_node = new_node
+	}
+
+	insert_node.next = link_node
 }
 
 func (list *LinkedList) listToSlice() []string {
