6e78cded71dde257e704adf0354688381dab3ed7
Gin <gkolomytcyn@gmail.com>
2025-03-31 15:07:24 +0200

    finishing day 14 part two

--- 14/main.go
+++ 14/main.go
@@ -13,17 +13,97 @@ func main() {
 
 	robots := readFile()
 
-	moves := 100
+	moves := 100000
 	x_max := 101
 	y_max := 103
-	// x_max := 11
-	// y_max := 7
 
 	quadrant_1 := 0
 	quadrant_2 := 0
 	quadrant_3 := 0
 	quadrant_4 := 0
 
+	part_one_output := 0
+
+	for i := 1; i <= moves; i++ {
+
+		quadrant_1 = 0
+		quadrant_2 = 0
+		quadrant_3 = 0
+		quadrant_4 = 0
+		output := createNewDisplayMatrix(x_max, y_max)
+		for _, robot := range robots {
+			robot.calculatePos(i, x_max, y_max)
+			robot_pos := robot.finalPos
+
+			// hate this
+			x := robot_pos[0]
+			y := robot_pos[1]
+			output[y][x]++
+
+			if x == x_max/2 || y == y_max/2 {
+				continue
+			}
+
+			if x < x_max/2 {
+				// either 1 or 3
+				if y < y_max/2 {
+					quadrant_1++
+				} else {
+					quadrant_3++
+				}
+			} else {
+				// either 2 or 4
+				if y < y_max/2 {
+					quadrant_2++
+				} else {
+					quadrant_4++
+				}
+			}
+		}
+
+		// not a clan solution, a bit of a cheat since it is known that answer has long continuous line
+		do_print := false
+		for _, o := range output {
+			str_out := ""
+			for _, p := range o {
+				if p == 0 {
+					str_out += "."
+				} else {
+					str_out += "#"
+				}
+			}
+
+			if strings.Contains(str_out, "##########") {
+				do_print = true
+			}
+		}
+
+		if do_print {
+			for _, o := range output {
+				str_out := ""
+				for _, p := range o {
+					if p == 0 {
+						str_out += "."
+					} else {
+						str_out += strconv.Itoa(p)
+					}
+				}
+				fmt.Println(str_out)
+			}
+
+			fmt.Printf("part two i: %d\r\n", i)
+			break
+		}
+
+		if i == 100 {
+			part_one_output = quadrant_1 * quadrant_2 * quadrant_3 * quadrant_4
+		}
+	}
+
+	fmt.Printf("part 1: %d\n", part_one_output)
+}
+
+func createNewDisplayMatrix(x_max int, y_max int) [][]int {
 	output := [][]int{}
 	for i := 0; i < y_max; i++ {
 		output = append(output, []int{})
@@ -31,59 +111,13 @@ func main() {
 			output[i] = append(output[i], 0)
 		}
 	}
-
-	// for _, o := range output {
-	// 	fmt.Println(o)
-	// }
-
-	for _, robot := range robots {
-		robot.calculatePos(moves, x_max, y_max)
-		robot_pos := robot.finalPos
-
-		// t := x_max / 2
-		// fmt.Println(t)
-
-		// hate this
-		x := robot_pos[0]
-		y := robot_pos[1]
-		output[y][x]++
-
-		if x == x_max/2 || y == y_max/2 {
-			continue
-		}
-
-		if x < x_max/2 {
-			// either 1 or 3
-			if y < y_max/2 {
-				quadrant_1++
-			} else {
-				quadrant_3++
-			}
-		} else {
-			// either 2 or 4
-			if y < y_max/2 {
-				quadrant_2++
-			} else {
-				quadrant_4++
-			}
-		}
-	}
-
-	fmt.Println("output")
-	// for _, o := range output {
-	// 	fmt.Println(o)
-	// }
-
-	fmt.Printf("part 1: %d\n", quadrant_1*quadrant_2*quadrant_3*quadrant_4)
-
-	// fmt.Println(quadrant_1, quadrant_2, quadrant_3, quadrant_4)
+	return output
 }
 
 func readFile() []*Robot {
 	output := []*Robot{}
 	FILE_NAME := "input.txt"
 	// FILE_NAME := "test.txt"
-	// FILE_NAME := "tmp.txt"
 
 	fi, err := os.Open(FILE_NAME)
 	if err != nil {
